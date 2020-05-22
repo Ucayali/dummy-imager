d9e1e83122923ab22d6459de1d8ce682162518f2
Adam Tiller <a.j.tiller@gmail.com>
2020-05-21 18:58:26 -0700

    Add resize task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,20 @@ const path = require('path');
 const axios = require('axios');
 const fs = require('fs');
 const Promise = require('bluebird');
+const jimp = require('jimp');
 
 module.exports = function(grunt) {
 
   grunt.initConfig({
     fullImage: path.join(__dirname, '/images/full/'),
+    mediumImage: path.join(__dirname, '/images/resized/'),
     tinyImage: path.join(__dirname, '/images/web/'),
-    push: {
-      bucket: '',
-    },
+    picList: [],
+    targetSize: [160, 160],
+    bucket: '',
   });
 
-  grunt.registerTask('makelist', 'Put together paged responses from Lorem Picsum insto a single array.', function() {
+  grunt.registerTask('makelist', 'Put together paged responses from Lorem Picsum into a single array.', function() {
     var done = this.async();
     var total = grunt.option('total');
 
@@ -23,10 +25,8 @@ module.exports = function(grunt) {
       const pages = total / 100;
 
       for (let i = 1; i <= pages; i++) {
-        console.log(i);
         const response = await axios.get(`https://picsum.photos/v2/list?page=${i}&limit=${total}`);
         grunt.config('picList', grunt.config('picList').concat(response.data));
-        console.log('list length:', grunt.config('picList').length);
       }
     }
 
@@ -48,7 +48,6 @@ module.exports = function(grunt) {
 
       return Promise.map(picList, (picData, index) => {
           async function getImage() {
-            //grunt.log.write(`Getting image ${index + 1}`).ok();
 
             const writer = fs.createWriteStream(`${grunt.config('fullImage')}${String(index + 1).padStart(4, '0')}.jpg`);
 
@@ -71,10 +70,44 @@ module.exports = function(grunt) {
     }
 
     getImages()
+      .catch((err) => { grunt.log.write(err).ok(); })
       .finally(done);
   });
 
-  grunt.registerTask('webify', 'Resize and compress images for web use.', function() {
+  grunt.registerTask('resize', 'Resize images for web use.', function() {
+    var done = this.async();
+
+    async function resizeImages() {
+      await fs.mkdir(grunt.config('mediumImage'), { recursive: true }, (err) => {
+        if (err) {
+          grunt.log.write(err).ok();
+        }
+      });
+
+      let fileArray = fs.readdirSync(grunt.config('fullImage'));
+
+      return Promise.map(fileArray, (filePath) => {
+          async function resizeImage() {
+            await jimp.read(grunt.config('fullImage') + filePath)
+            .then ((image) => {
+              image.resize(grunt.config('targetSize')[0], grunt.config('targetSize')[1])
+                .writeAsync(grunt.config('mediumImage') + filePath);
+            })
+            .catch((err) => { grunt.log.write(err).ok(); });
+          }
+
+          return resizeImage();
+        },
+        { concurrency: 10 }
+      );
+    }
+
+    resizeImages()
+      .catch((err) => { grunt.log.write(err).ok(); })
+      .finally(done);
+  });
+
+  grunt.registerTask('compress', 'Compress images for web use.', function() {
     var done = this.async();
     grunt.log.write('Logging some stuff...').ok();
     done();
@@ -86,6 +119,6 @@ module.exports = function(grunt) {
     done();
   });
 
-  grunt.registerTask('dummy', 'Download, process, and host dummy images', ['makelist','fetch', 'webify', 'push']);
+  grunt.registerTask('dummy', 'Download, process, and host dummy images', ['makelist','fetch', 'resize', 'compress', 'push']);
 
 };
